39deabcb0c5ca5b0b1ce49e444b9c94caf163c43
Zhephyr54 <ale.lanoix@gmail.com>
2016-11-27 20:42:41 +0100

    Déplacement des tests de collision de TestPlayer à TestGameMoveableElement

--- core/src/test/java/fr/univ_lorraine/spaceinvaders/model/TestGameMoveableElement.java
+++ core/src/test/java/fr/univ_lorraine/spaceinvaders/model/TestGameMoveableElement.java
@@ -4,16 +4,20 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TestGameMoveableElement {
 
     private GameMoveableElement gameMoveableElement;
 
+    private GameMoveableElement otherElement;
+
     private float delta;
 
     @Before
     public void setUp() {
-        gameMoveableElement = new GameMoveableElement(0f, 0f, 3f, 4f, 1f) {
+        gameMoveableElement = new GameMoveableElement(0f, 0f, 3f, 4f, 1f) { // speed = 1
             @Override
             public void handleCollision(GameElement element) {
             }
@@ -22,11 +26,24 @@ public class TestGameMoveableElement {
             public CollisionType getCollisionType() {
                 return null;
             }
-        };          // speed = 1
+        };
         gameMoveableElement.setIsMoving(true);
         delta = 1f; // deplacement = delta * speed = 1 * 1 = 1
+
+        otherElement = new GameMoveableElement() {
+            @Override
+            public CollisionType getCollisionType() {
+                return null;
+            }
+
+            @Override
+            public void handleCollision(GameElement element) {
+
+            }
+        };
     }
 
+    // Test methode update
     @Test
     public void testUpdateNotMoving() {
         gameMoveableElement.setIsMoving(false);
@@ -76,4 +93,31 @@ public class TestGameMoveableElement {
         assertEquals(0f, gameMoveableElement.getPosition().x, 0f);
         assertEquals(0f, gameMoveableElement.getPosition().y, 0f);
     }
+
+    //Test methode hasCollision
+    // On teste qu'un element ne detecte pas de collision avec un objet null
+    @Test
+    public void testHasNoCollisionWithNullObject() {
+        GameMoveableElement other = null;
+        assertFalse(gameMoveableElement.hasCollision(null));
+    }
+
+    // On teste qu'un element ne detecte pas de collision si non necessaire
+    @Test
+    public void testHasCollisionFalse() {
+        otherElement.init(gameMoveableElement); // On reprend les mêmes valeurs pour les attributs
+        // et on le positionne aux meme coordonnees juste au dessus du premier element
+        otherElement.setPosition(gameMoveableElement.getPosition().x, gameMoveableElement.getPosition().y + gameMoveableElement.getHeight() + 1);
+        assertFalse(gameMoveableElement.hasCollision(otherElement));
+        assertFalse(otherElement.hasCollision(gameMoveableElement));
+    }
+
+    // On teste qu'un element detecte bien une collision si necessaire
+    @Test
+    public void testHasCollisionTrue() {
+        otherElement.init(gameMoveableElement); // On reprend les mêmes valeurs pour les attributs
+        assertTrue(gameMoveableElement.hasCollision(otherElement));
+        assertTrue(otherElement.hasCollision(gameMoveableElement));
+    }
+
 }
